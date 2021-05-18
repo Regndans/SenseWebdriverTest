c6437d1b580a11b0d9292f2947b51b23db99637f
Morten Lyngberg Pedersen <mort73h9@edu.easj.dk>
2021-05-18 14:33:19 +0200

    Lavet ændre navn test

--- SenseWebdriverTest/EdgeDriverTest.cs
+++ SenseWebdriverTest/EdgeDriverTest.cs
@@ -58,23 +58,23 @@ namespace SenseWebdriverTest
             //Da vi havde problemer med at IWebElement hideTableButton refererede til en "forældet" instans, gentager vi processen med at sætte referencen 3 gange, for at give den øget chance for at virke.
             //for (int i = 0; i < 3; i++)
             //{
-                try
-                {
-                    //Der oprettes reference til tabellen.
-                    WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-                    IWebElement pirliste = wait.Until(d => d.FindElement(By.Id("pirListe")));
+            try
+            {
+                //Der oprettes reference til tabellen.
+                WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+                IWebElement pirliste = wait.Until(d => d.FindElement(By.Id("pirListe")));
 
-                    //Vi sætter reference til og trykker på hideTable-knappen på hjemmesiden.
-                    IWebElement hideTableButton = wait.Until(d => d.FindElement(By.Id("hideTableButton")));
-                    hideTableButton.Submit();
+                //Vi sætter reference til og trykker på hideTable-knappen på hjemmesiden.
+                IWebElement hideTableButton = wait.Until(d => d.FindElement(By.Id("hideTableButton")));
+                hideTableButton.Submit();
 
-                    Assert.IsFalse(pirliste.Text.Contains(textToFind));
-                }
-                catch (StaleElementReferenceException e)
-                {
-                    //Hvis metoden efter 3 forsøg, stadig ikke har en gyldig reference til hidetable-knappen, skal den fange en exception og udskrive den til consollen.
-                    Console.WriteLine(e);
-                }
+                Assert.IsFalse(pirliste.Text.Contains(textToFind));
+            }
+            catch (StaleElementReferenceException e)
+            {
+                //Hvis metoden efter 3 forsøg, stadig ikke har en gyldig reference til hidetable-knappen, skal den fange en exception og udskrive den til consollen.
+                Console.WriteLine(e);
+            }
             //}
         }
 
@@ -121,7 +121,7 @@ namespace SenseWebdriverTest
         [TestMethod]
         public void SensorButtonOnOffTest()
         {
-            
+
             WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
             IWebElement sensorOnButton = _driver.FindElement(By.Id("onButton"));
             IWebElement sensorOffButton = _driver.FindElement(By.Id("offButton"));
@@ -153,6 +153,30 @@ namespace SenseWebdriverTest
             Console.WriteLine("hej");
             Console.WriteLine(timerInputStart.Text);
             //Assert.IsTrue(timerInputStart.Text.Contains(DateTime.Now.Day.ToString()));
+
+        }
+
+        [TestMethod]
+        public void NameChangeTest()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+            var dropChangeNames = wait.Until(driver => driver.FindElement(By.Id("dropChangeNames")));
+            var inputChangeName = _driver.FindElement(By.Id("inputChangeName"));
+            var buttonChangeName = _driver.FindElement(By.Id("buttonChangeName"));
+            var selectElement = new SelectElement(dropChangeNames);
+            selectElement.SelectByText("Garden");
+            inputChangeName.Clear();
+            inputChangeName.SendKeys("have");
+            buttonChangeName.Submit();
+
+            string nameToFind = "have";
+            IWebElement sensorListe = wait.Until(d => d.FindElement(By.Id("sensorList")));
+            Assert.IsTrue(sensorListe.Text.Contains(nameToFind));
+
+            selectElement.SelectByText("have");
+            inputChangeName.Clear();
+            inputChangeName.SendKeys("Garden");
+            buttonChangeName.Submit();
 
         }
 
